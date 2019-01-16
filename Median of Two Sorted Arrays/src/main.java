65f016385887ecb86bc1576cee82597ae7e92642
TheZhuangPark <thezhuangPark@outlook.com>
2019-01-16 17:33:50 -0600

    1.0

--- Median of Two Sorted Arrays/src/main.java
+++ Median of Two Sorted Arrays/src/main.java
@@ -1,17 +1,6 @@
 import java.util.Random;
 import java.util.*;
 
-/*
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * */
-
 public class main {
   public static void main(String args[]) {
 	  System.out.println("Hello World");
