552f1da2731770c816b9a46c166bf8f96756667a
TheZhuangPark <thezhuangPark@outlook.com>
2019-01-28 14:21:22 -0600

    1.0

--- Longest palindromic Substring/src/main.java
+++ Longest palindromic Substring/src/main.java
@@ -4,4 +4,22 @@ public class main {
 	   System.out.println("project longest palindromic substring!");
 	   
    }
+   
+   public String longestPalindrome(String s) {
+       String result="";
+       int []p=new int[s.length()];
+       int center=0;
+       int len=1;
+       while(center<s.length()) {
+    	   for(int i=1;i<s.length();i++) {//compare center
+    		   if((center-i)>=0 &&(center+i)<s.length())
+    		   {
+    			   if(s[0]=) {
+    				   
+    			   }
+    		   }
+    	   }
+       }
+       return result;
+   }
 }
