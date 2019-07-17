f46de89366957240108dfd4b0a6f596d4ca8a9de
TheZhuangPark <thezhuangPark@outlook.com>
2019-07-16 20:29:32 -0500

    add Zigzag tree

--- Binary Tree Zigzag Level Order Traversal/src/com/company/Main.java
+++ Binary Tree Zigzag Level Order Traversal/src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root){
+        List<List<Integer>> res=new ArrayList<>();
+        travel(root,res,0);
+        return res;
+    }
+    private void travel(TreeNode node,List<List<Integer>>list,int level){
+        if(node==null)return ;
+        if(list.size()<=level){
+            List<Integer> newLevel= new ArrayList<>();
+            list.add(newLevel);
+        }
+        List<Integer> tmplist=list.get(level);
+        if(level%2==0) tmplist.add(node.val);
+        else tmplist.add(0,node.val);
+
+        travel(node.left,list,level+1);
+        travel(node.right,list,level+1);
     }
 }
